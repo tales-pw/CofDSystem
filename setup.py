906cc3e45a82a219c888c36efc48dd1a5312ce64
xunto <hunterdelyx1@gmail.com>
2021-03-21 02:53:28 +0300

    define pypi workflow

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='CofDSystem',
+    version='1.0.0',
+    author='xunto',
+    author_email='-',
+    py_modules=['CofDSystem'],
+    package_dir={'': 'out/python'}
+)
