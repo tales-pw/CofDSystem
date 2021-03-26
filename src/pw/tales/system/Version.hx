a1b928c4475f79862e46872abdfc40e7568c4e8d
Orlov Valentine <xunto@users.noreply.github.com>
2021-03-27 02:30:53 +0300

    Disable remote version check for dev build (#9)

--- src/pw/tales/system/Version.hx
+++ src/pw/tales/system/Version.hx
@@ -6,4 +6,10 @@ class Version {
         if (version == null) return macro "dev";
         return macro $v{version};
     }
+
+    public static macro function shouldCheck():haxe.macro.Expr.ExprOf<Bool> {
+        var version = Sys.getEnv("RELEASE_VERSION");
+        if (version == null) return macro false;
+        return macro true;
+    }
 }
