704f316b6422bcb6e6b02e8be8bb84c3cefb28ff
xunto <hunterdelyx1@gmail.com>
2021-04-25 02:25:44 +0300

    move git-specific things to git workflow

--- makefiles/pypi.mk
+++ makefiles/pypi.mk
@@ -1,18 +1,10 @@
-pypi_setup_keys: guard-SSH_AUTH_SOCK guard-PYPI_DEPLOY_KEY
-	ssh-agent -a $(SSH_AUTH_SOCK) > /dev/null
-	ssh-add - <<< "$(PYPI_DEPLOY_KEY)"
-
 pypi_setup_deps:
 	python3 -m pip install wheel
 
 pypi_build_package: build pypi_setup_deps
 	python3 setup.py bdist_wheel
 
-pypi_setup_git:
-	git config --local user.email "pypi@tales.pw"
-	git config --local user.name "pypi"
-
-pypi_publish: pypi_setup_keys pypi_setup_git pypi_build_package
+pypi_publish: pypi_build_package
 	git clone git@github.com:tales-pw/PyPI ./PyPI
 	cd PyPI
 
