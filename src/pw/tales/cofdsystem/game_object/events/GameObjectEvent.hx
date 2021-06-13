4cb47a74040db667d5f4092bf7b8bb2208bba63d
xunto <hunterdelyx1@gmail.com>
2021-06-14 01:18:11 +0300

    Add documentation

--- src/pw/tales/cofdsystem/game_object/events/GameObjectEvent.hx
+++ src/pw/tales/cofdsystem/game_object/events/GameObjectEvent.hx
@@ -3,6 +3,11 @@ package pw.tales.cofdsystem.game_object.events;
 import pw.tales.cofdsystem.game_object.events.IGameObjectEvent;
 import pw.tales.cofdsystem.game_object.GameObject;
 
+/**
+    Common class for all (most) GameObject-related events to extend from.
+    Never instantiated by itself, consider it abstract, avoid subscribing
+    to it, better use IGameObjectEvent, otherwise you may miss some events.
+**/
 class GameObjectEvent implements IGameObjectEvent {
     private final gameObject:GameObject;
 
