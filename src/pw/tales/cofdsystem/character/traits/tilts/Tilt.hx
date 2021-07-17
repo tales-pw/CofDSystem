f33c71e1d3f24d957146f1278ce79bf80779b558
xunto <hunterdelyx1@gmail.com>
2021-07-18 00:22:53 +0300

    Remove Tilts on Scene end.

--- src/pw/tales/cofdsystem/character/traits/tilts/Tilt.hx
+++ src/pw/tales/cofdsystem/character/traits/tilts/Tilt.hx
@@ -1,10 +1,21 @@
 package pw.tales.cofdsystem.character.traits.tilts;
 
+import pw.tales.cofdsystem.utils.events.HandlerPriority;
+import pw.tales.cofdsystem.scene.events.SceneEndEvent;
 import pw.tales.cofdsystem.game_object.GameObject;
 import pw.tales.cofdsystem.game_object.traits.Trait;
 
 class Tilt extends Trait {
     public function new(dn:String, gameObject:GameObject, type:TiltType) {
         super(dn, gameObject, type);
+        this.eventBus.addHandler(
+            SceneEndEvent,
+            this.applyInitiativeMod,
+            HandlerPriority.NORMAL
+        );
+    }
+
+    private function applyInitiativeMod(event:SceneEndEvent) {
+        this.gameObject.getTraitManager().removeTrait(this);
     }
 }
