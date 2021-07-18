9c1ba116ee66f5c28aeb352bf5568433a7dd3413
xunto <hunterdelyx1@gmail.com>
2021-07-18 03:47:10 +0300

    try different packaging

--- makefiles/npm.mk
+++ makefiles/npm.mk
@@ -1,4 +1,3 @@
 npm_publish: guard-NODE_AUTH_TOKEN build
-	echo $(NODE_AUTH_TOKEN)
-	yarn version --new-version 1.0.0 --no-git-tag-version
+	yarn version --new-version 1.0.0a1 --no-git-tag-version
 	yarn publish
