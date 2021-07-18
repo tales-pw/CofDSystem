01244f4283cd678167c6aaf9ce0cf281c955d886
xunto <hunterdelyx1@gmail.com>
2021-07-18 03:50:38 +0300

    Use release version variable

--- makefiles/npm.mk
+++ makefiles/npm.mk
@@ -1,3 +1,3 @@
 npm_publish: guard-NODE_AUTH_TOKEN build
-	yarn version --new-version 1.0.0a1 --no-git-tag-version
+	yarn version --new-version $(RELEASE_VERSION) --no-git-tag-version
 	yarn publish
