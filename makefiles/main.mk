b965ed3f3269ce6b0563a19484189cfed5852a08
xunto <hunterdelyx1@gmail.com>
2021-07-18 03:02:20 +0300

    separate builds

--- makefiles/main.mk
+++ makefiles/main.mk
@@ -5,14 +5,22 @@ setup_haxe:
 setup_npm:
 	npm install
 
-setup: setup_haxe setup_npm
-
 cleanup_build:
 	rm -rf out
 
-build: setup cleanup_build
+build_python: setup_haxe cleanup_build
+	haxe build_scripts/build_python.hxml
+
+build_java: setup_haxe cleanup_build
+	haxe build_scripts/build_java.hxml
+
+build_php: setup_haxe cleanup_build
+	haxe build_scripts/build_php.hxml
+
+build_js: setup_haxe setup_npm cleanup_build
 	npx webpack
-	haxe build_scripts/build.hxml
+
+build: cleanup_build build_js build_python build_php build_java
 
 docs: build
 	haxe build_scripts/docs.hxml
--- makefiles/npm.mk
+++ makefiles/npm.mk
@@ -1,4 +1,4 @@
-npm_publish: guard-NODE_AUTH_TOKEN build
+npm_publish: guard-NODE_AUTH_TOKEN build_js
 	yarn version --new-version 1.0.0 --no-git-tag-version
 	yarn install
 	yarn publish
