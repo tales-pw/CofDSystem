66b84948b1b04f19231c9ed16fd9ba791369b372
xunto <xunto.orlov@gmail.com>
2021-10-12 00:02:44 +0300

    Simplify concealed tags test.

--- test/pw/tales/cofdsystem/weapon/traits/weapon_tags/ConcealedTagTestCase.hx
+++ test/pw/tales/cofdsystem/weapon/traits/weapon_tags/ConcealedTagTestCase.hx
@@ -1,7 +1,7 @@
 package pw.tales.cofdsystem.weapon.traits.weapon_tags;
 
+import pw.tales.cofdsystem.game_object.GameObject;
 import pw.tales.cofdsystem.weapon.traits.weapon_tags.ConcealedTag;
-import haxe.Exception;
 import pw.tales.cofdsystem.action_attack.builder.AttackBuilder;
 import pw.tales.cofdsystem.common.EnumHand;
 import pw.tales.cofdsystem.character.traits.HeldWeapon;
@@ -14,46 +14,35 @@ class ConcealedTagTestCase extends CofDSystemTestCase
         return new WeaponPrefab("shield", "name", 0, 0, size, [ConcealedTag.TYPE]);
     }
 
-    private function setupc1(size:Int):Void
+    private function setupGO(gameObject:GameObject, size:Int):Void
     {
         var prefab = this.createShieldPrefab(size);
         var weapon = prefab.createWeapon(this.system);
 
-        var trait = this.c1.getTrait(HeldWeapon.TYPE);
+        var trait = gameObject.getTrait(HeldWeapon.TYPE);
         trait.setHand(EnumHand.HAND, weapon);
-    }
-
-    private function methodTestShield(size:Int)
-    {
-        this.setupc1(size);
-
-        var action = new AttackBuilder(c2, c1).build();
-        system.act(action);
-
-        var pool = action.getCompetition().getPool(c1);
-
-        if (pool == null)
-        {
-            throw new Exception("shouldn't be null");
-        }
-
-        var request = pool.getRequest();
-        var modifiers = request.getAppliedModifiers();
-
-        assertEquals(modifiers[ConcealedTag.TYPE.getDN()], size);
     }
 
     public function testConcealedAppliedWhenTargetWithShield()
     {
         for (size in 1...5)
         {
-            this.methodTestShield(size);
+            this.setupGO(c1, size);
+
+            var action = new AttackBuilder(c2, c1).build();
+            system.act(action);
+
+            var pool = action.getCompetition().getPool(c1);
+            var request = pool.getRequest();
+            var modifiers = request.getAppliedModifiers();
+
+            assertEquals(modifiers[ConcealedTag.TYPE.getDN()], size);
         }
     }
 
     public function testConcealedNotAppliedWhenActorWithShield()
     {
-        this.setupc1(3);
+        this.setupGO(c1, 3);
 
         var action = new AttackBuilder(c1, c2).build();
         system.act(action);
