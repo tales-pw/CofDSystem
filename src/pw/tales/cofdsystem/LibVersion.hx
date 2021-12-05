52fe2dd425909fcbd7ecd3f4b9d3d20ee7cf49bb
xunto <xunto.orlov@gmail.com>
2021-12-05 21:22:54 +0300

    Fix vscode can't build cache because of missing env variables.

--- src/pw/tales/cofdsystem/LibVersion.hx
+++ src/pw/tales/cofdsystem/LibVersion.hx
@@ -4,15 +4,21 @@ import thx.semver.Version;
 
 class LibVersion
 {
+    #if macro
+    private static function getVersionFromEnv(): String {
+        return Sys.getEnv("RELEASE_VERSION");
+    }
+    #end
+
     public static macro function get():haxe.macro.Expr.ExprOf<String>
     {
-        var version = Sys.getEnv("RELEASE_VERSION");
+        var version = LibVersion.getVersionFromEnv();
         return macro $v{version};
     }
 
     public static macro function shouldCheck():haxe.macro.Expr.ExprOf<Bool>
     {
-        var version:Version = Sys.getEnv("RELEASE_VERSION");
+        var version:Version = LibVersion.getVersionFromEnv();
 
         if (version.hasPre)
             return macro false;
