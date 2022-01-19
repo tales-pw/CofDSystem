feb3d408cc0157e9de3a2fee884ac604981c24a1
xunto <xunto.orlov@gmail.com>
2022-01-19 23:45:18 +0300

    Add GameObjectSerialization#applyChanges.

--- src/pw/tales/cofdsystem/synchronization/serialization/game_object/GameObjectSerialization.hx
+++ src/pw/tales/cofdsystem/synchronization/serialization/game_object/GameObjectSerialization.hx
@@ -1,5 +1,6 @@
 package pw.tales.cofdsystem.synchronization.serialization.game_object;
 
+import thx.Set;
 import pw.tales.cofdsystem.utils.serialization.DeserializationException;
 import haxe.Json;
 import pw.tales.cofdsystem.game_object.traits.unknown_trait.UnknownTrait;
@@ -38,14 +39,16 @@ class GameObjectSerialization extends Serialization<GameObject, GameObjectData>
         return manager.addTrait(type, dn);
     }
 
-    private function getRemovedTraits(gameObject:GameObject, data:GameObjectData):Array<Trait>
+    private function getRemovedTraits(gameObject:GameObject, data:GameObjectData):Array<String>
     {
         var oldTraits = gameObject.getTraitManager().getTraits().items();
-        var newDNs = [for (trait in data.traits) trait.dn];
-
-        return oldTraits.filter(
-            (v:Trait) -> !newDNs.contains(v.getDN())
+        var oldDns = Set.createString(
+            [for (trait in oldTraits) trait.getDN()]
         );
+        var newDNs = Set.createString([
+            for (traitData in data.traits) traitData.dn
+        ]);
+        return (oldDns - newDNs).toArray();
     }
 
     public override function createNewObj(data:GameObjectData):GameObject
@@ -55,50 +58,68 @@ class GameObjectSerialization extends Serialization<GameObject, GameObjectData>
 
     public override function updateWithData(gameObject:GameObject, data:GameObjectData):GameObject
     {
+        var logger = LoggerManager.getLogger();
+        logger.info('Loading ${gameObject}.');
+
         gameObject.setState(GameObjectState.LOADING);
 
         // Start updating given GameObject
         gameObject.version = data.version;
 
-        var traits:Array<Dynamic> = data.traits;
-        var manager = gameObject.getTraitManager();
-
         // Handle removed traits
         var removedTraits = this.getRemovedTraits(gameObject, data);
-        for (trait in removedTraits)
-            manager.removeTrait(trait);
 
-        var logger = LoggerManager.getLogger();
-        // Handle new and updated traits
-        for (trait_data in traits)
-        {
-            var type:TraitType<Dynamic> = system.traits.getRecord(trait_data.type);
-            if (type == null)
-            {
-                type = cast(UnknownTrait.TYPE);
-                logger.warning(
-                    'Unable to identify trait type for data: ${Json.stringify(trait_data)} fallback to ${type}'
-                );
-            }
-
-            var trait:Trait = this.ensureTrait(manager, type, trait_data.dn);
-            try
-            {
-                trait.deserialize(trait_data);
-            } catch (e:DeserializationException)
-            {
-                logger.warning(
-                    'Unable to create ${trait.getType()} from data ${Json.stringify(trait_data)} because: ${e.message}.'
-                );
-                manager.removeTrait(trait);
-                trait = manager.addTrait(UnknownTrait.TYPE);
-                trait.deserialize(trait_data);
-            }
-        }
+        this.applyChanges(gameObject, data.traits, removedTraits);
 
         // Make GameObject active
         gameObject.setState(GameObjectState.ACTIVE);
+        logger.info('${gameObject} loaded.');
+
         return gameObject;
+    }
+
+    public function applyChanges(
+        gameObject:GameObject,
+        update:Array<Dynamic>,
+        remove:Array<String>
+    ):Void
+    {
+        var logger = LoggerManager.getLogger();
+        var manager = gameObject.getTraitManager();
+
+        // Remove traits
+        for (dn in remove)
+        {
+            var trait = manager.getTraitByDn(dn);
+            manager.removeTrait(trait);
+        }
+
+        // Handle new and updated traits
+        for (traitData in update)
+        {
+            var type:TraitType<Dynamic> = system.traits.getRecord(traitData.type);
+            if (type == null)
+            {
+                type = UnknownTrait.TYPE;
+                logger.warning(
+                    'Unable to identify trait type for data: ${Json.stringify(traitData)} fallback to ${type}'
+                );
+            }
+
+            var trait:Trait = this.ensureTrait(manager, type, traitData.dn);
+            try
+            {
+                trait.deserialize(traitData);
+            } catch (e:DeserializationException)
+            {
+                logger.warning(
+                    'Unable to create ${trait.getType()} from data ${Json.stringify(traitData)} because: ${e.message}.'
+                );
+                manager.removeTrait(trait);
+                trait = manager.addTrait(UnknownTrait.TYPE);
+                trait.deserialize(traitData);
+            }
+        }
     }
 
     public override function toData(gameObject:GameObject):GameObjectData
