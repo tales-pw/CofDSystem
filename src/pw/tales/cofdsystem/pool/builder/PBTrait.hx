83171c36e1af5e2aee9a454fe710a38856aa28d0
Valentin Orlov <v.orlov@reg.ru>
2022-03-14 17:57:22 +0300

    Return PoolTrait for now.

--- src/pw/tales/cofdsystem/pool/builder/PBTrait.hx
+++ src/pw/tales/cofdsystem/pool/builder/PBTrait.hx
@@ -1,8 +1,7 @@
 package pw.tales.cofdsystem.pool.builder;
 
-import pw.tales.cofdsystem.utils.math.MathValue;
-import pw.tales.cofdsystem.game_object.traits.Trait;
 import pw.tales.cofdsystem.game_object.traits.TraitType;
+import pw.tales.cofdsystem.pool.math.PoolTrait;
 import pw.tales.cofdsystem.game_object.GameObject;
 import pw.tales.cofdsystem.utils.math.IMathOperation;
 
@@ -22,19 +21,8 @@ class PBTrait extends PoolBuilder
         return this.traitType.getDisplayName();
     }
 
-    private function getTraitValue(gameObject:GameObject):Int
-    {
-        var trait:Null<Trait> = gameObject.getTrait(this.traitType);
-
-        if (trait == null)
-            return 0;
-
-        return trait.getValue();
-    }
-
     override public function build(gameObject:GameObject):IMathOperation<Int>
     {
-        var value = this.getTraitValue(gameObject);
-        return new MathValue(value);
+        return new PoolTrait(gameObject, this.traitType);
     }
 }
---
+++ src/pw/tales/cofdsystem/pool/math/PoolTrait.hx
@@ -0,0 +1,42 @@
+package pw.tales.cofdsystem.pool.math;
+
+import pw.tales.cofdsystem.game_object.traits.Trait;
+import pw.tales.cofdsystem.game_object.traits.TraitType;
+import pw.tales.cofdsystem.game_object.GameObject;
+import pw.tales.cofdsystem.utils.math.IMathOperation;
+
+@:expose("PoolTrait")
+class PoolTrait implements IMathOperation<Int>
+{
+    private final gameObject:GameObject;
+    private var traitType:TraitType<Dynamic>;
+
+    public function new(gameObject:GameObject, traitType:TraitType<Dynamic>)
+    {
+        this.gameObject = gameObject;
+        this.traitType = traitType;
+    }
+
+    public function getTraitType():TraitType<Dynamic>
+    {
+        return this.traitType;
+    }
+
+    public function setTraitType(traitType:TraitType<Dynamic>):Void
+    {
+        this.traitType = traitType;
+    }
+
+    public function getOperands():Array<IMathOperation<Dynamic>>
+    {
+        return [];
+    }
+
+    public function calculate():Int
+    {
+        var trait:Null<Trait> = this.gameObject.getTrait(this.traitType);
+        if (trait == null)
+            return 0;
+        return trait.getValue();
+    }
+}
