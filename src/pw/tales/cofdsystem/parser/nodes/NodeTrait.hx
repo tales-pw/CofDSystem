b5a9ae9498c1d42a9d6f7c2c4ef70523f0aff9c8
Valentin Orlov <v.orlov@reg.ru>
2022-03-14 17:55:12 +0300

    Use builder in NodeTrait

--- src/pw/tales/cofdsystem/parser/nodes/NodeTrait.hx
+++ src/pw/tales/cofdsystem/parser/nodes/NodeTrait.hx
@@ -1,6 +1,6 @@
 package pw.tales.cofdsystem.parser.nodes;
 
-import pw.tales.cofdsystem.pool.math.PoolTrait;
+import pw.tales.cofdsystem.pool.builder.PBTrait;
 import pw.tales.cofdsystem.game_object.GameObject;
 import pw.tales.cofdsystem.utils.math.IMathOperation;
 
@@ -27,7 +27,7 @@ class NodeTrait implements INode implements INodePoolBuilder
     public function build(gameObject:GameObject):IMathOperation<Int>
     {
         var traitType = gameObject.getSystem().traits.getRecord(this.dn);
-        return new PoolTrait(gameObject, traitType);
+        return new PBTrait(traitType).build(gameObject);
     }
 
     public function toString():String
